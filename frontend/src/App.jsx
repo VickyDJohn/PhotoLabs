c74fe625f74b1ef053c23690f0c3b44177c980d5
Vicky John <vicky.d.john@email.com>
2023-08-17 21:18:00 -0700

    import and add PhotoList, TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  // const dataArray = new Array(3).fill(sampleDataForPhotoListItem);
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <TopicList />
+      <PhotoList />
     </div>
   );
 };
