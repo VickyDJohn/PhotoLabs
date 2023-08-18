d95da380877583c3b758489508b6881204b89dbe
Vicky John <vicky.d.john@email.com>
2023-08-17 21:15:14 -0700

    use props, changed code to use object data from PhotoList

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ sampleData }) => {
+  return (
+    <div className="photo-list__item">
+      <PhotoFavButton />
+      <img className="photo-list__image" src={sampleData.urls.regular} />
+      <div className="photo-list__user-alldetails">
+        <img className="photo-list__user-profile" src={sampleData.user.profile} />
+        <div className="photo-list__user-details">
+          <div className="photo-list__user-info">{sampleData.user.username}</div>
+          <div className="photo-list__user-location">
+            {sampleData.location.city} {sampleData.location.country}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
