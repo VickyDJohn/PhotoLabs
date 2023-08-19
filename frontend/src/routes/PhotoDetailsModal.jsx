02ff7a41871456d957eb1d2cbe599d56d1589bb4
Vicky John <vicky.d.john@email.com>
2023-08-19 15:05:04 -0700

    add code to dsiplay zoomed in photo

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ closeModal, selectedPhoto, similarPhotos }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div className="photo-details-modal__content">
+        <img src={selectedPhoto.urls.full} alt={selectedPhoto.id} />
+        <div>{selectedPhoto.user.username}</div>
+        <div>{selectedPhoto.location.city}, {selectedPhoto.location.country}</div>
+        <div>Similar Photos:</div>
+        <div className="similar-photos">
+          {similarPhotos.map((photo) => (
+            <img key={photo.id} src={photo.urls.regular} alt={photo.id} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
