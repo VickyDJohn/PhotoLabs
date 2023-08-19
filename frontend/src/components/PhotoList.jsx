c636d28d735e74118759fc27a6a44a4f57408628
Vicky John <vicky.d.john@email.com>
2023-08-19 15:03:05 -0700

    add onclick event handler

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,11 +3,11 @@ import PhotoListItem from './PhotoListItem';
 import '../styles/PhotoList.scss';
 import '../styles/PhotoListItem.scss';
 
-const PhotoList = ({ photos, favoritedPhotos, onFavToggle }) => {
+const PhotoList = ({ photos, favoritedPhotos, onFavToggle, onPhotoClick }) => {
   return (
     <ul className="photo-list">
       {photos.map((data) => (
-        <div key={data.id} className="photo-list__item">
+        <div key={data.id} className="photo-list__item" onClick={() => onPhotoClick(data)}>
           <PhotoListItem sampleData={data} onFavToggle={() => onFavToggle(data.id)} isFavorited={favoritedPhotos.includes(data.id)} />
         </div>
       ))}
