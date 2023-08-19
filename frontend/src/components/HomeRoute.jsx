6e26b6f2dd050e8b646b9d32caae67b0e06e033e
Vicky John <vicky.d.john@email.com>
2023-08-19 15:02:20 -0700

    added functionality to click photo

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -3,11 +3,11 @@ import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = ({ photos, topics, favoritedPhotos, onFavToggle }) => {
+const HomeRoute = ({ photos, topics, favoritedPhotos, onFavToggle, onPhotoClick }) => {
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics} favoritedPhotos={favoritedPhotos} />
-      <PhotoList photos={photos} favoritedPhotos={favoritedPhotos} onFavToggle={onFavToggle} />
+      <PhotoList photos={photos} favoritedPhotos={favoritedPhotos} onFavToggle={onFavToggle} onPhotoClick={onPhotoClick} />
     </div>
   );
 };
