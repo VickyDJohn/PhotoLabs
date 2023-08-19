89b5a7080a06e734d805dba5caef96331472e155
Vicky John <vicky.d.john@email.com>
2023-08-19 15:05:37 -0700

    add modal handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import HomeRoute from 'components/HomeRoute';
-import photos from 'mocks/photos'; // Import the mock photos
-import topics from 'mocks/topics'; // Import the mock topics
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 import './App.scss';
 
 function App() {
   const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null); // State to track the selected photo
+  const [similarPhotos, setSimilarPhotos] = useState([]); // State to track similar photos
 
   const toggleFav = (photoId) => {
     if (favoritedPhotos.includes(photoId)) {
@@ -15,9 +19,32 @@ function App() {
     }
   };
 
+  const openModal = (photo) => {
+    setSelectedPhoto(photo); // Set the selected photo
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedPhoto(null); // Clear the selected photo
+    setModalOpen(false);
+  };
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} favoritedPhotos={favoritedPhotos} onFavToggle={toggleFav} />
+      <HomeRoute
+        photos={photos}
+        topics={topics}
+        favoritedPhotos={favoritedPhotos}
+        onFavToggle={toggleFav}
+        onPhotoClick={openModal} // Pass the openModal function as the click handler
+      />
+      {modalOpen && (
+        <PhotoDetailsModal
+          closeModal={closeModal}
+          selectedPhoto={selectedPhoto}
+          similarPhotos={similarPhotos}
+        />
+      )}
     </div>
   );
 }
