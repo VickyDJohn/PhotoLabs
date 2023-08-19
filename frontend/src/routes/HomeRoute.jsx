5da67a7c9a7e520bea75c95ec97860b7ef54cf63
Vicky John <vicky.d.john@email.com>
2023-08-19 15:12:48 -0700

    changed destination of HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import TopNavigationBar from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = ({ photos, topics, favoritedPhotos, onFavToggle, onPhotoClick }) => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar topics={topics} favoritedPhotos={favoritedPhotos} />
+      <PhotoList photos={photos} favoritedPhotos={favoritedPhotos} onFavToggle={onFavToggle} onPhotoClick={onPhotoClick} />
     </div>
   );
 };
