153b8572ee97ca129dc4ace86f5ead04a8bb8fcf
Vicky John <vicky.d.john@email.com>
2023-08-19 09:46:45 -0700

    add fav photo tracking

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
 
-const TopNavigationBar = () => {
+const TopNavigationBar = ({ topics, favoritedPhotos }) => {
+  const isFavPhotoExist = favoritedPhotos.length > 0;
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
-      <FavBadge isFavPhotoExist={true} />
+      <TopicList topics={topics} />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
-  )
+  );
 }
 
 export default TopNavigationBar;
