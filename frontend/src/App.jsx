1e9c59f3efa11b80c806e187867437aa9efecb5d
Vicky John <vicky.d.john@email.com>
2023-08-19 09:47:30 -0700

    add conditional to check if fav photo exists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
+import React, { useState } from 'react';
 import HomeRoute from 'components/HomeRoute';
+import photos from 'mocks/photos'; // Import the mock photos
+import topics from 'mocks/topics'; // Import the mock topics
 import './App.scss';
 
+function App() {
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
 
-// Note: Rendering a single component to build components in isolation
-const App = () => {
-  // const dataArray = new Array(3).fill(sampleDataForPhotoListItem);
+  const toggleFav = (photoId) => {
+    if (favoritedPhotos.includes(photoId)) {
+      setFavoritedPhotos(favoritedPhotos.filter((id) => id !== photoId));
+    } else {
+      setFavoritedPhotos([...favoritedPhotos, photoId]);
+    }
+  };
 
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={photos} topics={topics} favoritedPhotos={favoritedPhotos} onFavToggle={toggleFav} />
     </div>
   );
-};
+}
 
 export default App;
