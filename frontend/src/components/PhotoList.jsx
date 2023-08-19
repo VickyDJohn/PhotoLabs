aa0f34654f38b538653c6ca3fd27010dfa930408
Vicky John <vicky.d.john@email.com>
2023-08-19 09:45:30 -0700

    add functionality for FavBadge

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,69 +1,14 @@
-import React from "react";
-import PhotoListItem from "./PhotoListItem";
-import "../styles/PhotoList.scss";
-import "../styles/PhotoListItem.scss"
-import photos from "mocks/photos";
+import React from 'react';
+import PhotoListItem from './PhotoListItem';
+import '../styles/PhotoList.scss';
+import '../styles/PhotoListItem.scss';
 
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
-
-const PhotoList = () => {
+const PhotoList = ({ photos, favoritedPhotos, onFavToggle }) => {
   return (
     <ul className="photo-list">
       {photos.map((data) => (
         <div key={data.id} className="photo-list__item">
-          <PhotoListItem sampleData={data} />
+          <PhotoListItem sampleData={data} onFavToggle={() => onFavToggle(data.id)} isFavorited={favoritedPhotos.includes(data.id)} />
         </div>
       ))}
     </ul>
