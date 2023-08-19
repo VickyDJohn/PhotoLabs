de5e5fe56ddc20fa1e6e8af47283f629c8dd134a
Vicky John <vicky.d.john@email.com>
2023-08-19 09:44:04 -0700

    add props

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -3,12 +3,11 @@ import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
-
+const HomeRoute = ({ photos, topics, favoritedPhotos, onFavToggle }) => {
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics} favoritedPhotos={favoritedPhotos} />
+      <PhotoList photos={photos} favoritedPhotos={favoritedPhotos} onFavToggle={onFavToggle} />
     </div>
   );
 };
