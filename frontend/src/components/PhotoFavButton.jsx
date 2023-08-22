76eccfe4720348935e88661abd09f6d21e77ff46
Vicky John <vicky.d.john@email.com>
2023-08-22 15:48:55 -0700

    change event propagation

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,10 +3,14 @@ import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton({ isFavorited, onFavToggle }) {
+  const handleFavToggleClick = (e) => {
+    e.stopPropagation(); // Stops the event from bubbling up
+    onFavToggle();
+  };
   return (
     <div
-      className={`photo-list__fav-icon ${isFavorited ? "active" : ""}`}
-      onClick={onFavToggle}
+      className={`photo-list__fav-icon`}
+      onClick={handleFavToggleClick}
     >
       <div className="photo-list__fav-icon-svg">
         <FavIcon selected={isFavorited} />
